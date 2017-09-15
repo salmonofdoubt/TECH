c748384262dfa62e1fbd66c0b615732428928ef8
Andre Baumann <andre.c.baumann@gmail.com>
2017-09-15 15:54:09 +0100

    mac02

--- PROG/go/g3train/StdLibArgs1.go
+++ PROG/go/g3train/StdLibArgs1.go
@@ -21,7 +21,7 @@ import "fmt"
 
 func main() {
 
-	//These are defaults / mac02
+	//These are defaults / mac03
 	wordPtr := flag.String("word", "foo", "a string")
 	numbPtr := flag.Int("num", 42, "an int")
 	boolPtr := flag.Bool("fork", false, "a bool")
