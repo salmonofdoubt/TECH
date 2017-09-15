1445f1c9eca31eec6341754ca234fc37b19af239
Andre Baumann <andre.c.baumann@gmail.com>
2017-09-15 10:20:13 +0100

    modified:   PROG/go/g3train/StdLibArgs1.go

--- PROG/go/g3train/StdLibArgs1.go
+++ PROG/go/g3train/StdLibArgs1.go
@@ -21,7 +21,7 @@ import "fmt"
 
 func main() {
 
-	//These are defaults
+	//These are defaults / new commit
 	wordPtr := flag.String("word", "foo", "a string")
 	numbPtr := flag.Int("num", 42, "an int")
 	boolPtr := flag.Bool("fork", false, "a bool")
