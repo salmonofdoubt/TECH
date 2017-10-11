ef0777e1f74a0ce8e39391fcdb38e79c40a3aae3
Andre Baumann <andre.c.baumann@gmail.com>
2017-10-11 11:46:05 +0100

    new file:   PROG/go/g3train/StdLibPathBasic.go 	modified:   PROG/go/learn/stdlib/pathBasic.go

--- PROG/go/g3train/StdLibPathBasic.go
+++ PROG/go/g3train/StdLibPathBasic.go
@@ -1,6 +1,7 @@
 // author: abaumann@google.com
 // From go/gonotes Jan 17 2017
-
+// cetus
+// fedramp 
 /*
 Standard library:
 	path			: manipulates only linux like OS path seperator
@@ -24,8 +25,9 @@ func main() {
 
 	myPath := "/google/data/ro/projects/netops/gdrain.sh"
 
-	fmt.Println("Abs filepath:", path.IsAbs(myPath))
-	fmt.Println("Name:        ", path.Base(myPath))
-	fmt.Println("Ext:         ", path.Ext(myPath))
-	fmt.Println("Dir:         ", path.Dir(myPath))
+	fmt.Println("Abs filepath or not?", path.IsAbs(myPath))
+	fmt.Println("Name=", path.Base(myPath))
+	fmt.Println("Ext=", path.Ext(myPath))
+	fmt.Println("Dir=", path.Dir(myPath))
+	fmt.Println("bye")
 }
