34f75e9223c7a9aa87eb119932d82d37be6cc9e7
Andre Baumann <andre.c.baumann@gmail.com>
2021-04-08 18:37:57 +0100

    SQL wildcards

--- SQL/W3-SQL.sql
+++ SQL/W3-SQL.sql
@@ -53,3 +53,27 @@ WHERE CustomerID = 32;
 ----------------------------------------------
 DELETE FROM Customers WHERE CustomerName='Alfreds Futterkiste';
 
+----------------------------------------------
+SELECT * FROM Customers LIMIT 3;    //mySQL
+
+SELECT * FROM Customers WHERE Country='Germany' LIMIT 3;
+
+----------------------------------------------
+SELECT MIN(Price) AS SmallestPrice FROM Products; 
+
+SELECT COUNT(*) FROM Products WHERE Price = 18;
+
+----------------------------------------------
+SELECT * FROM Customers WHERE City NOT LIKE 'a%';
+
+SELECT * FROM Customers WHERE City LIKE '[!bsp]%';
+
+'a%'	Finds any values that start with "a"
+'%a'	Finds any values that end with "a"
+'%or%'	Finds any values that have "or" in any position
+'_r%'	Finds any values that have "r" in the second position
+'a_%'	Finds any values that start with "a" and are at least 2 characters in length
+'a__%'	Finds any values that start with "a" and are at least 3 characters in length
+'a%o'	Finds any values that start with "a" and ends with "o"
+
+
